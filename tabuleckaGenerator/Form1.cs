bf6f02388b61d04852262731fa27abd9d04ef8b6
xmatakt <timotej.matak@gmail.com>
2017-02-01 21:18:30 +0100

    vytvorenie uvodnej app

---
+++ tabuleckaGenerator/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabuleckaGenerator
+{
+    public partial class Form1 : Form
+    {
+        private TabuleckasGenerator generator = null;
+
+        public Form1()
+        {
+            this.generator = new TabuleckasGenerator(2, 2017);
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generator.CreateHeaders(1,1,"HEllo WOrld");
+            generator.SaveDocument(@"C:\AATimo\daco.xlsx");
+        }
+    }
+}
---
+++ tabuleckaGenerator/TabuleckasGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop;
+using System.Windows.Forms;
+
+namespace tabuleckaGenerator
+{
+    class TabuleckasGenerator
+    {
+        private Microsoft.Office.Interop.Excel.Application excelApp = null;
+        private Microsoft.Office.Interop.Excel.Workbook workbook = null;
+        private Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+        private int month;
+        private int year;
+
+        public TabuleckasGenerator(int month, int year)
+        {
+            this.month = month;
+            this.year = year;
+
+            CreateDocument();
+        }
+
+        private void CreateDocument()
+        {
+            try
+            {
+                excelApp = new Microsoft.Office.Interop.Excel.Application();
+                excelApp.Visible = true;
+                workbook = excelApp.Workbooks.Add(1);
+
+                worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Sheets[1];
+                worksheet.Name = month + "_" + year;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            excelApp = new Microsoft.Office.Interop.Excel.Application();
+        }
+
+        public void CreateHeaders(int row, int col, string text)
+        {
+            worksheet.Cells[row, col] = text;
+            
+        }
+
+        public void SaveDocument(string filePath)
+        {
+            workbook.SaveAs(filePath);
+            workbook.Close(0);
+            excelApp.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+        }
+    }
+}
